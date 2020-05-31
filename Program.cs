79e9ab99fad8299c7d55679fa8dc847832a09f52
banebanane <banebanane993@gmail.com>
2020-05-31 18:13:19 +0200

    proba

--- Program.cs
+++ Program.cs
@@ -1,12 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using LungHypertensionApp.Data;
 
 namespace LungHypertensionApp
@@ -16,12 +11,16 @@ namespace LungHypertensionApp
         public static void Main(string[] args)
         {
             var host = CreateHostBuilder(args);
-
             RunSeeding(host);
-
-          //  host.Build().Run();
-          //  CreateHostBuilder(args).Build().Run();
         }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(SetupConfiguration)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
 
         private static void RunSeeding(IHostBuilder host)
         {
@@ -35,20 +34,11 @@ namespace LungHypertensionApp
             seederBuilded.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration(SetupConfiguration)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-
         private static void SetupConfiguration(HostBuilderContext ctx, IConfigurationBuilder builder)
         {
             builder.Sources.Clear();
             builder.AddJsonFile("config.json", false, true)
-               // .AddXmlFile("config.xml", true)
-                .AddEnvironmentVariables(); // o ovome treba voditi racuna prilikom deploy-a. Prednsot ima poslednji dodat ako ima konflikta medju parametrima
+                   .AddEnvironmentVariables(); // o ovome treba voditi racuna prilikom deploy-a. Prednsot ima poslednji dodat ako ima konflikta medju parametrima
         }
     }
 }
